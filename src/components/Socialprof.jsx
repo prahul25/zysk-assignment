b38e76125784dbdb0b078c5fb784c94b45ff082f
prahul25 <palrahulkumar2002@gmail.com>
2024-09-19 00:20:36 +0530

    Added Social prof section

--- src/components/Socialprof.jsx
+++ src/components/Socialprof.jsx
@@ -1,8 +1,38 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Socialprof() {
   return (
-    <div>Socialprof</div>
+    <div className='flex flex-col items-center py-14'>
+      <div className='text-[#475467] tracking-wide '>Join 4,000+ companies already growing</div>
+      <div className='flex gap-6 py-8'>
+        <div className='flex gap-2 items-center text-2xl font-semibold'>
+          <Image src="/Boltshift.png" height={40} width={40} alt='Boltshift'/>
+          Boltshift
+        </div>
+        <div className='flex gap-2 items-center text-2xl font-semibold'>
+          <Image src="/Lighthouse.png" height={40} width={40} alt='Boltshift'/>
+          Lightbox
+        </div>
+        <div className='flex gap-2 items-center text-2xl font-semibold'>
+          <Image src="/FeatherDev.png" height={40} width={40} alt='Boltshift'/>
+          FeatherDev
+        </div>
+        <div className='flex gap-2 items-center text-2xl font-semibold'>
+          <Image src="/spehrule.png" height={40} width={40} alt='Boltshift'/>
+          Spherule
+        </div>
+        <div className='flex gap-2 items-center text-2xl font-semibold'>
+          <Image src="/GlobalBank.png" height={40} width={40} alt='Boltshift'/>
+          GlobalBank
+        </div>
+        <div className='flex gap-2 items-center text-2xl font-semibold'>
+          <Image src="/Neitze.png" height={40} width={40} alt='Boltshift'/>
+          Nietzsche
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
