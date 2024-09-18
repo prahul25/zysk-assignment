7faaad5a3e6139eb27a4fb69d8c91f9826e0c849
prahul25 <palrahulkumar2002@gmail.com>
2024-09-19 00:36:17 +0530

    Made banner image responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,18 +12,23 @@ function Hero() {
         <div><FaArrowRightLong/></div>
       </div>
       <div className='md:text-6xl text-4xl font-semibold text-center'>Beautiful analytics to grow smarter</div>
-      <div className='text-[#475467] text-lg w-6/12 text-center'>Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.</div>
-      <div className='flex gap-4 py-4'>
-      <button className='px-6 py-3 flex justify-center gap-2 items-center border border-gray-200 rounded-lg'>
+      <div className='text-[#475467] text-lg px-4 md:w-6/12 text-center'>Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.</div>
+      <div className='flex md:flex-row justify-center flex-col gap-4 py-4 w-full px-4'>
+      <button className='px-6 py-3 flex justify-center w-full md:w-28 gap-2 items-center border border-gray-200 rounded-lg'>
         <CgPlayButtonO/>
         Demo
       </button>
-      <button className='px-6 py-3 flex justify-center gap-2 items-center bg-[#7F56D9] text-white rounded-lg'>
+      <button className='px-6 py-3 flex justify-center w-full md:w-28 gap-2 items-center bg-[#7F56D9] text-white rounded-lg'>
         Sign up
       </button>
       </div>
-      <div>
-      <Image src="/Content.png" alt='Banner' layout='responsive' height={500} width={1200} quality={100} />
+      <div className='hidden md:block'>
+        <Image src="/Content.png" alt='Desktop Banner' layout='responsive' height={500} width={1200} quality={100} />
+      </div>
+
+      {/* Mobile Image (Visible on screens smaller than md) */}
+      <div className='block md:hidden'>
+        <Image src="/Container.png" alt='Mobile Banner' layout='responsive' height={500} width={600} quality={100} />
       </div>
     </div>
   )
